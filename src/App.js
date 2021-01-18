7ec4b316350b6009e7eee3be9f694b8417ddde1f
Musa Ali <musa6ali@gmail.com>
2021-01-18 12:01:30 -0500

    Remove Particles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import "./App.css";
 import Header from "./components/Header";
 import About from "./components/About";
-import Particles from "react-particles-js";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -12,60 +12,7 @@ function App() {
       <About />
       <Experience />
       <Projects />
-
-      <Particles
-        params={{
-          "particles": {
-            "number": {
-              "value": 160,
-              "density": {
-                "enable": false
-              }
-            },
-            "size": {
-              "value": 3,
-              "random": true,
-              "anim": {
-                "speed": 4,
-                "size_min": 0.3
-              }
-            },
-            "line_linked": {
-              "enable": false
-            },
-            "move": {
-              "random": true,
-              "speed": 1,
-              "direction": "top",
-              "out_mode": "out"
-            }
-          },
-          "interactivity": {
-            "events": {
-              "onhover": {
-                "enable": true,
-                "mode": "bubble"
-              },
-              "onclick": {
-                "enable": true,
-                "mode": "repulse"
-              }
-            },
-            "modes": {
-              "bubble": {
-                "distance": 250,
-                "duration": 2,
-                "size": 0,
-                "opacity": 0
-              },
-              "repulse": {
-                "distance": 400,
-                "duration": 4
-              }
-            }
-          }
-        }}
-      />
+      <Footer />
     </div>
   );
 }

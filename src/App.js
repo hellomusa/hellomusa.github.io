6a8ebab00b5e97d1f799fdfd0d011d943238c983
Musa Ali <musa6ali@gmail.com>
2021-01-18 16:51:25 -0500

    Add environment variable

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from "./components/About";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
 import Footer from "./components/Footer";
+require('dotenv').config();
 
 function App() {
   return (
